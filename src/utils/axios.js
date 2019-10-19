afef30392a4b7974d30cf6b03327081fb05d444a
debanjan pal <paldeababrata82@gmail.com>
2019-10-19 14:53:24 +0530

    production setup

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
+import {baseURL} from './config';
 
 export default axios.create({
-  baseURL: `http://localhost/api`
+  baseURL: baseURL+'/api',
 });
--- src/utils/config.js
+++ src/utils/config.js
@@ -1,3 +1,6 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
 export const api = {
-  baseURL: 'http://localhost',
+  baseURL: process.env.NODE_ENV=='production' ? 'http://34.207.94.86:4000' : 'http://localhost:4000',
 }
